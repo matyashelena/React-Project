8e9ffaae6824db4d90af1b030b52ff24ca2d428d
matyashelena <avrora.solar@gmail.com>
2023-11-18 11:52:44 +0200

    add ctabs card

--- src/App.js
+++ src/App.js
@@ -1,106 +1,86 @@
 import logo_header from './images/header_logo.svg';
 import icon_search from './images/search.png';
 import icon_thermometer from './images/thermometer.svg'
-import icon_humidity from './images/humidity.svg';
-import icon_evaporator from './images/evaporator.svg';
+import icon_humidity from './images/humidity_icon.png';
+import icon_evaporator from './images/precipitation.png';
 import icon_wind from './images/wind.svg';
-import clouds from './images/clouds.png';
-import humidity from './images/humidity.png';
-import rain from './images/rain.png';
-import clear from './images/clear.png';
-import snow from './images/snow.png';
-import drizzle from './images/drizzle.png';
+import clouds from './images/clouds1.png';
+import humidity from './images/humidity1.png';
+import rain from './images/rain1.png';
+import clear from './images/clear1.png';
+import haze from './images/haze.png'
+import snow from './images/snow1.png';
+import drizzle from './images/drizzle1.png';
+import fog from './images/fog.png'
+import sunrise_icon from './images/sunrise.png';
+import sunset_icon from './images/sunset.png';
+import pop_icon from './images/pop.png';
+import min_temp from './images/min_temp.png';
+import max_temp from './images/max_temp1.png';
+// import TimePicker from 'react-time-picker';
+
 import './App.css';
 import moment from 'moment';
-import Moment from 'react-moment';
-import React, { useState } from 'react'
+// import Moment from 'react-moment';
+import React, { useEffect, useState } from 'react'
 
 const API_KEY = 'aaa362ec11316d74bc1716de935b46d2';
 const API_URL="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const API_URL_DAILY ="https://api.openweathermap.org/data/2.5/forecast?";
 const options = {
   method: 'GET'
 }
 function App() {
   return (
-    <div className="App page_wrapper">
+    <div className="App page_wrapper dark">
       <Page/>
+      <BgBlock/>
     </div>
+  )
+}
+
+function BgBlock() {
+  return (
+    <div className="bg_url"></div>
   )
 }
 
 function Page() {
   const [value, setValue] = useState('');
   const [weather, setWeather] = useState({});
+  const [lon, setLon] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [weatherdayly, setWeatherdayly] = useState({});
+
+  useEffect(() => {
+    getWeatherDaily();
+  }, [weather, lon, lat])
+
+  function getWeatherDaily() {
+    if (!lon || !lat) {
+      return false
+    }
+    let localUrlDaily = `${API_URL_DAILY}lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(localUrlDaily, options)
+    .then((res) => res.json())
+    .then((response) => {
+      response ? setWeatherdayly(response) : setWeatherdayly ({});
+      console.log(response);
+    })
+    .catch(err => console.error(err)) 
+  }
 
   const searchHandler = () => {
     let localUrl = `${API_URL}${value}&appid=${API_KEY}`;
-    // let lon = weather.coord.lon;
-    // let lat = weather.coord.lat;
-    // let fiveDaysUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    // sata(fiveDaysUrl)
-    // console.log(fiveDaysUrl);
     fetch(localUrl, options)
       .then((res) => res.json())
       .then((response) => {
-       response.weather ? setWeather(response) : setWeather ({})
-        console.log(response);
-        console.log(weather);
-        console.log(response.weather);
-        // console.log(lon);
-        // console.log(lat);
-        
+       response ? setWeather(response) : setWeather ({})
+       console.log(response);
+        setLon(response.coord.lon);
+        setLat(response.coord.lat);
       })
       .catch(err => console.error(err))
-
-
-    //   fetch(fiveDaysUrl, options)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     // Filter the forecasts to get only one forecast per day
-    //     console.log(data);
-    //     console.log(data.list);
-
-
-    //     const uniqueForecastDays = [];
-    //     const fiveDaysForecast = data.list.filter(forecast => {
-    //         const forecastDate = new Date(forecast.dt_txt).getDate();
-    //         console.log(forecastDate);
-    //         if (!uniqueForecastDays.includes(forecastDate)) {
-    //             return uniqueForecastDays.push(forecastDate);
-    //         }
-    //         console.log(uniqueForecastDays);
-    //     });
-
-        
-
-    //     // Creating weather cards and adding them to the DOM
-    //     fiveDaysForecast.forEach((weatherItem, index) => {
-    //         const html = createWeatherCard(cityName, weatherItem, index);
-    //         console.log(index);
-    //         console.log(fiveDaysForecast);
-    //         if (index === 0) {
-    //             currentWeatherDiv.insertAdjacentHTML("beforeend", html);
-    //         } else {
-    //             weatherCardsDiv.insertAdjacentHTML("beforeend", html);
-    //         }
-    //     });        
-    // }).catch(() => {
-    //     alert("An error occurred while fetching the weather forecast!");
-    // });
-
-
-    //   fetch(fiveDaysUrl, options)
-    //   .then((res) => res.json())
-    //   .then((response) => {
-    //    response.weather ? setWeather(response) : setWeather ({})
-    //     console.log(response);
-    //     console.log(weather);
-    //     console.log(response.weather);
-    //     console.log(lon);
-    //     console.log(lat);
-        
-    //   })
-    //   .catch(err => console.error(err))
   }
 
   return (
@@ -112,8 +92,12 @@ function Page() {
 
         {typeof weather.main !== "undefined" 
         ? <Main weather={weather}/> 
-        : <p className='invalid_name'>enter valid city name</p>}
+        : <p className='invalid_name '>Enter valid name</p>}
 
+        {typeof weatherdayly.list !== 'undefined'
+        ? <DailyWeather weather={weather} weatherdayly={weatherdayly}/>
+        : null}
+        
       </div>
   )
 }
@@ -155,7 +139,7 @@ function Input(props) {
     props.onInputChange && props.onInputChange(e.target.value);
   }
   return (
-    <input onChange={inputHandler} type={props.type} placeholder={props.placeholder} value={props.value} spellCheck="false"/>
+    <input className='search_input' onChange={inputHandler} type={props.type} placeholder={props.placeholder} value={props.value.trimLeft()} spellCheck="false"/>
   )
 }
 
@@ -179,11 +163,14 @@ function Main(props) {
     case 'Clouds':
       icon_weather = clouds;
       break;
-    case 'Smoke' || 'Haze':
+    case 'Smoke':
       icon_weather = humidity;
       break;
+    case 'Fog':
+      icon_weather = fog;
+      break;  
     case 'Haze':
-      icon_weather = humidity;
+      icon_weather = haze;
       break;
     case 'Rain':
       icon_weather = rain;
@@ -197,20 +184,20 @@ function Main(props) {
     case 'Drizzle':
       icon_weather = drizzle;
       break;
+    case 'Mist':
+      icon_weather = humidity;
+      break;
     default:
       icon_weather = `https://openweathermap.org/img/w/${props.weather.weather[0].icon}.png`;
       break;
   }
   return (
-
-    <main weather={props.weather} className="cards_today">
+    <div weather={props.weather} className="cards_today">
         <div className="card card_city">
           <div className="weather_header">
             <div className="weather_main">
               <div className="weather_temp">{props.weather.main.temp.toFixed()}°</div>
-              <div className="weather_today">Today is: 
-                <p className='moment'>{moment().format('dddd')}</p> 
-                
+              <div className='moment'>{moment().format('dddd')} 
               </div>
             </div>
             <div className="weather_icon">
@@ -253,9 +240,127 @@ function Main(props) {
           </div>
           
         </div>
-        </main>
+    </div>
   )
 }
 
+function DailyWeather(props) {
+  const [toggle, setToggle] = useState(1);
+  const  toggleTab = (index) => {
+    setToggle(index);
+  }
+
+  
+  const sunrise = props.weather.sys.sunrise;
+  const sunriseRender = new Date(sunrise*1000).toLocaleTimeString();
+  const sunset = props.weather.sys.sunset;
+  const sunsetRender = new Date(sunset*1000).toLocaleTimeString();
+  
+  return (
+    <div>
+      <div className="block_tabs">
+        <div className={toggle === 1 ? "tabs active" : "tabs"} onClick={() => toggleTab(1)}>more details for today</div>
+        <div className={toggle === 2 ? "tabs active" : "tabs"} onClick={() => toggleTab(2)}>for the next 24 hours</div>
+        <div className={toggle === 3 ? "tabs active" : "tabs"} onClick={() => toggleTab(3)}>for 5 days</div>       
+      </div>
+
+      <div className={toggle === 1 ? "cards_dayly active" : "cards_dayly"}>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>Sunrise</h3>
+          <p className='card_dayly-date'>{sunriseRender}</p>
+            <img className='card_dayly-img' src={sunrise_icon} alt="" />        
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>Sunset</h3>
+          <p className='card_dayly-date'>{sunsetRender}</p>
+          <img className='card_dayly-img' src={sunset_icon} alt="" />
+        </div>
+        <div className="card_dayly">
+          <p className='card_dayly-date'>Precipitation probability: {props.weatherdayly.list[0].pop}%</p>
+          <img className='card_dayly-img' src={pop_icon} alt="" />
+        </div>
+        <div className="card_dayly">
+          <p className='card_dayly-date'>Minimum temperature: {props.weather.main.temp_min.toFixed(1)}°C</p>
+          <img className='card_dayly-img' src={min_temp} alt="" />
+        </div>
+        <div className="card_dayly">
+          <p className='card_dayly-date'>Maximum temperature: {props.weather.main.temp_max.toFixed(1)}°C</p>
+          <img className='card_dayly-img' src={max_temp} alt="" />
+        </div>
+      </div>
+    
+      <div className={toggle === 2 ? "cards_dayly active" : "cards_dayly"}>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[0].dt_txt).slice(0, -9)}</h3>
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[0].dt_txt).slice(11)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[0].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[0].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[0].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[1].dt_txt).slice(0, -9)}</h3>
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[1].dt_txt).slice(11)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[8].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[1].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[1].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[2].dt_txt).slice(0, -9)}</h3>
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[2].dt_txt).slice(11)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[16].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[2].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[2].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[3].dt_txt).slice(0, -9)}</h3>
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[3].dt_txt).slice(11)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[24].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[3].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[3].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[4].dt_txt).slice(0, -9)}</h3>
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[4].dt_txt).slice(11)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[32].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[4].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[4].weather[0].description}</p>
+        </div>
+      </div>
+
+      <div className={toggle === 3 ? "cards_dayly active" : "cards_dayly"}>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[0].dt_txt).slice(0, -9)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[0].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[0].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[0].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[8].dt_txt).slice(0, -9)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[8].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[8].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[8].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[16].dt_txt).slice(0, -9)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[16].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[16].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[16].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[24].dt_txt).slice(0, -9)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[24].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[24].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[24].weather[0].description}</p>
+        </div>
+        <div className="card_dayly">
+          <h3 className='card_dayly-date'>{(props.weatherdayly.list[32].dt_txt).slice(0, -9)}</h3>
+          <img className='card_dayly-icon' src={`https://openweathermap.org/img/w/${props.weatherdayly.list[32].weather[0].icon}.png`} alt="" />
+          <p className='card_dayly-date'>{(props.weatherdayly.list[32].main.temp - 273.15).toFixed()}°C</p>
+          <p className='card_dayly-desc'>{props.weatherdayly.list[32].weather[0].description}</p>
+        </div>
+      </div>
+    </div>
+  )
+} 
 
 export default App;
